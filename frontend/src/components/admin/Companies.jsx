c6eb82e517fe5d20ac06994f5a12bcb3e1e43b14
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-07-15 21:57:06 +0530

    Update Companies.jsx

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -16,12 +16,30 @@ function Companies() {
     dispatch(setSearchbycompany(input));
   });
   return (
+  import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import CompaniesTable from "./CompaniesTable";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setSearchbycompany } from "@/redux/companySlice";
+import { ComputerIcon } from "lucide-react";
+
+function Companies() {
+  const navigate = useNavigate();
+  const [input, setinput] = useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setSearchbycompany(input));
+  });
+  return (
     <div className="text-white  overflow-hidden">
       <Navbar />
-      <div className="max-w-6xl mx-auto my-10 ">
-        <div className="flex items-center justify-between">
+      <div className="max-w-6xl p-2 lg:p-0 mx-auto my-5 sm:mt-10 ">
+        <div className="flex gap-3 items-center justify-between">
           <Input
-            className="w-full max-w-sm border-[#2b384a]"
+            className="w-full max-w-sm border-[#42608a]"
             placeholder="Filter by name"
             onChange={(e) => setinput(e.target.value)}
           />
@@ -37,6 +55,11 @@ function Companies() {
         </div>
       </div>
     </div>
+  );
+}
+
+export default Companies;
+
   );
 }
 
