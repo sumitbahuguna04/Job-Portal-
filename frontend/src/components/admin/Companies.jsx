3214d9861df8354738804365a6225fecee9d3ae4
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-07-16 10:25:09 +0530

    Update Companies.jsx

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -12,41 +12,25 @@ function Companies() {
   const navigate = useNavigate();
   const [input, setinput] = useState("");
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(setSearchbycompany(input));
-  });
-  return (
-  import React, { useEffect, useState } from "react";
-import Navbar from "../shared/Navbar";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import CompaniesTable from "./CompaniesTable";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setSearchbycompany } from "@/redux/companySlice";
-import { ComputerIcon } from "lucide-react";
 
-function Companies() {
-  const navigate = useNavigate();
-  const [input, setinput] = useState("");
-  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setSearchbycompany(input));
-  });
+  }, [input, dispatch]);
+
   return (
-     <div className="text-white  overflow-hidden">
+    <div className="text-white overflow-hidden">
       <Navbar />
-      <div className="max-w-6xl p-5   md:p-5  lg:p-0 mx-auto my-5 sm:mt-10 ">
+      <div className="max-w-6xl p-5 md:p-5 lg:p-0 mx-auto my-5 sm:mt-10">
         <div className="flex gap-3 items-center justify-between">
           <Input
             className="w-full max-w-sm border-[#42608a]"
             placeholder="Filter by name"
             onChange={(e) => setinput(e.target.value)}
           />
-
           <Button
             onClick={() => navigate("/admin/companies/create")}
-            className="border border-[#222f3e] hover:bg-[#1F2937] hover:border-[#1F2937] text-white transition  cursor-pointer duration-200">
+            className="border border-[#222f3e] hover:bg-[#1F2937] hover:border-[#1F2937] text-white transition cursor-pointer duration-200"
+          >
             Create New Company
           </Button>
         </div>
@@ -55,11 +39,6 @@ function Companies() {
         </div>
       </div>
     </div>
-  );
-}
-
-export default Companies;
-
   );
 }
 
