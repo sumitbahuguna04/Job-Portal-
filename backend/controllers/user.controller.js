9d0efc7e7e39d9ada7dc8bff108760d838ed8b76
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-07-18 20:47:08 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -68,8 +68,8 @@ export const login = async (req, res) => {
 
    res.cookie("token", token, {
   httpOnly: true,
-  secure: true,        // ✅ needed for HTTPS
-  sameSite: "None",    // ✅ needed for cross-origin cookies
+  secure: true,       
+  sameSite: "None",    
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 });
 
