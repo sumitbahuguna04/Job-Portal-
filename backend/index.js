b8b20aa80e7dcefbf1de18bea6c0f5bed5a26037
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-07-20 21:20:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,18 +23,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
-app.use(cors(corsOptions));
-
-
-app.use(cors(corsOptions));
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
