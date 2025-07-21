f2e3cacd35bd16ef7fe7a16b13b8981464288d3e
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-07-21 12:17:24 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -164,3 +164,12 @@ export const updateProfile = async (req, res) => {
     console.log(error);
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
