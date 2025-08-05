55da5900b355c95c676c5053213c77b55b6aad75
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-08-05 20:41:55 +0530

    Update LatestJobs.jsx

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -8,7 +8,7 @@ function LatestJobs() {
   return (
     <div className="text-white max-w-7xl px-5 sm:px-24 mb-23">
       <h1 className="text-3xl sm:text-4xl font-bold ">
-        <span className="text-[#4f46e5]">Latest & Top </span>Jobs Openings
+        <span className="text-[#7653c8]">Latest & Top </span>Jobs Openings
       </h1>
       <div className="my-5 grid sm:grid-cols-3 gap-6">
         {allJobs.length <= 0 ? <span>No jobs found</span> : allJobs.slice(0, 6).map((job) => (
