08d99b35653aea4b8aa44abb40dd2de70f78d38e
Sumit Bahuguna <162596847+sumitbahuguna04@users.noreply.github.com>
2025-08-06 20:22:58 +0530

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -16,13 +16,13 @@ const dispatch = useDispatch();
      }
   })
   return (
-    <div className="text-white ">
+        <div className="text-white ">
       <Navbar />
       <div className=" max-w-7xl m-auto mt-10 mb-10">
-        <h1 className="font-bold text-xl">
-          Search Results {searchedjobArray.length}
+        <h1 className="font-bold text-xl px-5 sm:px-2">
+          Search Results: {searchedjobArray.length}
         </h1>
-        <div className="grid mt-4 grid-cols-3 gap-4">
+        <div className="grid  px-5 sm:px-2 mt-4 sm:grid-cols-3 gap-4">
           {searchedjobArray.map((job ) => {
             return <Job job={job}  />;
           })}
